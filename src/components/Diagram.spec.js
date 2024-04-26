295adec40d7a3ac00a478ad28d650350c2011f26
bishweswar2024 <168172936+bishweswar2024@users.noreply.github.com>
2024-04-26 13:46:24 +0530

    Create Diagram.spec.js

---
+++ src/components/Diagram.spec.js
@@ -0,0 +1,7 @@
+import { mount } from "@vue/test-utils"
+import Diagram from "./Diagram.vue"
+describe("Diagram", () => {
+    it("renders properly", () => {
+        const wrapper = mount(Diagram)
+    })
+})
